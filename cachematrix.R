cc79e77994e612820546393ccb7a11ef9079aa28
ezeschwab <ezequiel.schwab@gmail.com>
2014-05-23 23:20:39 -0300

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,23 +1,23 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Here, we create the special matrix just as the example in the special vector
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    m <<- NULL ##As it is a new value that it is set, we set the mean back to null
   }
   get <- function() x
-  setinv <- function(newInv) inv <<- newInv
+  setinv <- function(newInv) inv <<- newInv ##We assign the new value
   getinv <- function() inv
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
 }
 
-## Write a short comment describing this function
+## Here, we create the cache so that we don't have to calculate again the inverse if it was already calculated
 
 cachesolve <- function(x, ...) {
   inv <- x$getinv()
@@ -26,7 +26,7 @@ cachesolve <- function(x, ...) {
     return(inv)
   }
   data <- x$get()
-  inv <- solve(data, ...)
+  inv <- solve(data, ...) ##We call the solve function as recommended in the assignment for calculating the inverse
   x$setinv(inv)
   inv
 }
